8cd86aac845857a066a46aaa650a145adb173ecf
Jaroslav Rusanov <sco.nuff@gmail.com>
2023-12-20 11:33:17 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -2,8 +2,13 @@
 
 import { program } from "commander";
 
+const gendiff = (first, second, format) => null;
+
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .argument('<first>', 'path to file 1')
+  .argument('<second>', 'path to file 2')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
