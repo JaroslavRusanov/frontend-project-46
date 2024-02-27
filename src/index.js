b83b0163246340cd1a98cb4f8245a86dfa724dd1
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-02-27 17:19:05 +0300

    mentors code-review 4

--- src/index.js
+++ src/index.js
@@ -22,17 +22,22 @@ const dataTree = (path1, path2) => {
     const tree = getSortedKeys(data1, data2).reduce((acc, key) => {
       if (!Object.hasOwn(data2, key)) {
         acc[key] = { changing: 'deleted', value: data1[key] };
+        return acc;
       }
       if (!Object.hasOwn(data1, key)) {
         acc[key] = { changing: 'added', value: data2[key] };
+        return acc;
       }
       if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
         if (_.isObject(data1[key]) && _.isObject(data2[key])) {
           acc[key] = iter(data1[key], data2[key]);
+          return acc;
         } else if (data1[key] === data2[key]) {
           acc[key] = { changing: 'unchanged', value: data1[key] };
+          return acc;
         } else {
           acc[key] = { changing: 'changed', value1: data1[key], value2: data2[key] };
+          return acc;
         }
       }
       return acc;
