ef6254902c38728a239a898fea9d740878295319
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-02-28 14:02:31 +0300

    mentors code-review 8

--- src/diffTree.js
+++ src/diffTree.js
@@ -8,24 +8,23 @@ const getSortedKeys = (data1, data2) => {
 export default (file1, file2) => {
   const iter = (data1, data2) => {
     const tree = getSortedKeys(data1, data2).reduce((acc, key) => {
-      let result = {};
+      const newAcc = { ...acc };
       if (!Object.hasOwn(data2, key)) {
-        result = { changing: 'deleted', value: data1[key] };
+        newAcc[key] = { changing: 'deleted', value: data1[key] };
       }
       if (!Object.hasOwn(data1, key)) {
-        result = { changing: 'added', value: data2[key] };
+        newAcc[key] = { changing: 'added', value: data2[key] };
       }
       if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
         if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-          result = iter(data1[key], data2[key]);
+          newAcc[key] = iter(data1[key], data2[key]);
         } else if (data1[key] === data2[key]) {
-          result = { changing: 'unchanged', value: data1[key] };
+          newAcc[key] = { changing: 'unchanged', value: data1[key] };
         } else {
-          result = { changing: 'changed', value1: data1[key], value2: data2[key] };
+          newAcc[key] = { changing: 'changed', value1: data1[key], value2: data2[key] };
         }
       }
-      acc[key] = result;
-      return acc;
+      return newAcc;
     }, {});
     return tree;
   };
