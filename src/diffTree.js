6d9ce92c941a480ba9f68a70beab5c8f528b24d8
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-02-28 16:02:34 +0300

    mentors code-review 11

--- src/diffTree.js
+++ src/diffTree.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import _ from 'lodash';
 
 const getSortedKeys = (data1, data2) => {
@@ -12,18 +10,18 @@ export default (file1, file2) => {
     const tree = getSortedKeys(data1, data2).reduce((acc, key) => {
       const newAcc = { ...acc };
       if (!Object.hasOwn(data2, key)) {
-        newAcc[key] = { changing: 'deleted', value: data1[key] };
+        newAcc[key] = { changing: 'deleted', value: data1[key] };// eslint-disable-line
       }
       if (!Object.hasOwn(data1, key)) {
-        newAcc[key] = { changing: 'added', value: data2[key] };
+        newAcc[key] = { changing: 'added', value: data2[key] };// eslint-disable-line
       }
       if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
         if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-          newAcc[key] = iter(data1[key], data2[key]);
+          newAcc[key] = iter(data1[key], data2[key]);// eslint-disable-line
         } else if (data1[key] === data2[key]) {
-          newAcc[key] = { changing: 'unchanged', value: data1[key] };
+          newAcc[key] = { changing: 'unchanged', value: data1[key] };// eslint-disable-line
         } else {
-          newAcc[key] = { changing: 'changed', value1: data1[key], value2: data2[key] };
+          newAcc[key] = { changing: 'changed', value1: data1[key], value2: data2[key] };// eslint-disable-line
         }
       }
       return newAcc;
